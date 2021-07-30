48714221681ebb7731db7e024161d4874d14d954
Bryan <bryantello2010@hotmail.com>
2021-07-30 17:16:31 -0600

    Added general settings on sass

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { router } from './router/index.routes';
 
+// Importing styles with sass
+import './main.sass'
+
 router(window.location.hash)
 
 window.addEventListener('hashchange', () => {
