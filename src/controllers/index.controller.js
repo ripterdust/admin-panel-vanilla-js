a810de10b3fdb4dc1ffd8a8f2e8735fbbc6c8efe
Bryan <bryantello2010@hotmail.com>
2021-07-31 00:06:35 -0600

    Added quantities

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,10 +1,23 @@
 import view from '../views/index.view.html';
 
+
+const getTotalData = (component) => {
+
+    let numbers = component.querySelectorAll('.number');
+
+    numbers.forEach(element => {
+        element.innerHTML = 1
+    });
+}
+
+
 export const Index = () => {
 
     const component = document.createElement('div');
     component.classList.add('index-screen');
     component.innerHTML = view;
+
+    getTotalData(component);
 
 
 
