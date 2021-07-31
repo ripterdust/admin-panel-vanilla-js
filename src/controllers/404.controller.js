9a898408ccdd1c367f29b16adcb7e537273a2509
Bryan <bryantello2010@hotmail.com>
2021-07-31 15:44:03 -0600

    Added sass file to edit 404 page

--- src/controllers/404.controller.js
+++ src/controllers/404.controller.js
@@ -1,11 +1,11 @@
 import view from '../views/404.view.html';
-
+import '../styles/404styles.scss';
 
 export const NotFound404 = () => {
 
     const container = document.createElement('div');
-    container.classList.add('flex-col')
-    container.classList.add('h-2/4')
+    container.classList.add('notFound')
+
     container.innerHTML = view;
 
     return container
