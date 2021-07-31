79704803f6920e84a5ebfdec8ab59f94f9b16646
Bryan <bryantello2010@hotmail.com>
2021-07-30 23:40:40 -0600

    Added index cards

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -3,7 +3,10 @@ import view from '../views/index.view.html';
 export const Index = () => {
 
     const component = document.createElement('div');
+    component.classList.add('index-screen');
     component.innerHTML = view;
+
+
 
     return component;
 }
