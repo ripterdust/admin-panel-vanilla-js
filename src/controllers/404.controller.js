3f5c41ce3e7918bfe3f3542ca6f821bff8134e8c
Bryan <bryantello2010@hotmail.com>
2021-07-31 15:49:32 -0600

    Trying to fix h bug

--- src/controllers/404.controller.js
+++ src/controllers/404.controller.js
@@ -4,8 +4,7 @@ import '../styles/404styles.scss';
 export const NotFound404 = () => {
 
     const container = document.createElement('div');
-    container.classList.add('notFound')
-
+    container.classList.add('notFound');
     container.innerHTML = view;
 
     return container
