43bb32f290191e3b84c5653c0821a884682849cb
Bryan <bryantello2010@hotmail.com>
2021-07-31 15:35:30 -0600

    fixing 404 layout

--- src/controllers/404.controller.js
+++ src/controllers/404.controller.js
@@ -4,6 +4,8 @@ import view from '../views/404.view.html';
 export const NotFound404 = () => {
 
     const container = document.createElement('div');
+    container.classList.add('flex-col')
+    container.classList.add('h-2/4')
     container.innerHTML = view;
 
     return container
