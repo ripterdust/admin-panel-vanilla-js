68cebe4f2298189bffd88ed35adef5f1f5f39510
Bryan <bryantello2010@hotmail.com>
2021-08-03 13:36:54 -0600

    Added agent page, search and styles pending

--- src/controllers/agent.controller.js
+++ src/controllers/agent.controller.js
@@ -1,21 +1,31 @@
 import axios from 'axios';
 import view from '../views/agents.view.html';
 
-const getData = async (component) => {
+const getData = async () => {
 
     let url = 'http://localhost:3000/agents';
 
     const { data } = await axios(url);
 
-    console.log(data)
+    let { agents } = data
+    
+    let agentsDiv = document.querySelector('#agentsList');
 
+    agents.map(({ id, firstname, lastname }) => {
+        agentsDiv.innerHTML += `<tr class="text-center hover:bg-gray-50">
+            <td class="p-1">${id}</td>
+            <td class="p-1">${firstname}</td>
+            <td class="p-1">${lastname}</td>
 
+        </tr>`
+    });
 }
 
 export const Agents = () => {
 
     let container = document.createElement('div');
     container.innerHTML = view;
+    getData()
     return container;
 
 }
