438f7fbed03ebd8c1f51d67806fe8d61bddf814b
Bryan <bryantello2010@hotmail.com>
2021-08-03 14:25:39 -0600

    Searchbar working

--- src/controllers/agent.controller.js
+++ src/controllers/agent.controller.js
@@ -3,17 +3,12 @@ import view from '../views/agents.view.html';
 import '../styles/agents.scss';
 
 
-const getData = async () => {
-
-    let url = 'http://localhost:3000/agents';
-
-    const { data } = await axios(url);
-
-    let { agents } = data
-    
+var allData = [];
+const renderAgents = (data) => {
     let agentsDiv = document.querySelector('#agentsList');
 
-    agents.map(({ id, firstname, lastname }) => {
+    agentsDiv.innerHTML = '';
+    data.map(({ id, firstname, lastname }) => {
         agentsDiv.innerHTML += `<tr class="text-center hover:bg-gray-50">
             <td class="p-1">${id}</td>
             <td class="p-1">${firstname}</td>
@@ -21,13 +16,40 @@ const getData = async () => {
 
         </tr>`
     });
+
 }
 
-export const Agents = () => {
+const getData = async (render = true) => {
 
+    let url = 'http://localhost:3000/agents';
+
+    const { data } = await axios(url);
+    var { agents } = data;
+    renderAgents(agents)
+    allData = [...agents]
+    console.log(allData)
+};
+
+
+export const Agents = () => {
     let container = document.createElement('div');
+
     container.innerHTML = view;
     getData()
+
+    let searchBar = container.querySelector('#searchBar');
+    searchBar.addEventListener('keyup', ({target}) => {
+        
+        let filteredData = allData.filter(({firstname, lastname }) => {
+
+            if(firstname.includes(target.value) || lastname.includes(target.value)){
+                return true
+            }
+
+        })
+
+        renderAgents(filteredData)
+    })
     return container;
 
 }
