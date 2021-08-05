aca34abe19706c0410fdb31d47076d9f28036220
Bryan <bryantello2010@hotmail.com>
2021-08-05 13:05:10 -0600

    Added delete agent button, onclick function is not working

--- src/controllers/agent.controller.js
+++ src/controllers/agent.controller.js
@@ -4,35 +4,56 @@ import '../styles/agents.scss';
 
 
 var allData = [];
+
+
 const renderAgents = (data) => {
     let agentsDiv = document.querySelector('#agentsList');
-
+    
     agentsDiv.innerHTML = `<tr class="bg-white">
-        <th class="p-2">
-            Id
-        </th>
-        <th class="p-2">
-            First name
-        </th>
-        <th class="p-2">
-            Last name
-        </th>
+    <th class="p-2">
+    Id
+    </th>
+    <th class="p-2">
+    First name
+    </th>
+    <th class="p-2">
+    Last name
+    </th>
+    <th class="p-2">
+    Action
+    </th>
     </tr>`;
     data.map(({ id, firstname, lastname }) => {
         agentsDiv.innerHTML += `<tr class="text-center hover:bg-gray-50">
-            <td class="p-1">${id}</td>
-            <td class="p-1">${firstname}</td>
-            <td class="p-1">${lastname}</td>
-
+        <td class="p-1">${id}</td>
+        <td class="p-1">${firstname}</td>
+        <td class="p-1">${lastname}</td>
+        <td class="p-1">
+        <button 
+        id="delete" 
+        class="p-2 bg-red-500 rounded-full text-white"
+        agentId='${id}'
+        onclick="deleteAgent()"
+        >
+            Delete Agent
+        </button>
+        </td>
+        
         </tr>`
     });
 
+    const deleteAgent = (id = 1) => {
+        console.log(id)
+    }
 }
 
-const getData = async (render = true) => {
 
+
+
+const getData = async () => {
+    
     let url = 'http://localhost:3000/agents';
-
+    
     const { data } = await axios(url);
     var { agents } = data;
     renderAgents(agents)
@@ -41,23 +62,23 @@ const getData = async (render = true) => {
 
 export const Agents = () => {
     let container = document.createElement('div');
-
+    
     container.innerHTML = view;
     getData()
-
+    
     let searchBar = container.querySelector('#searchBar');
     searchBar.addEventListener('keyup', ({target}) => {
         
         let filteredData = allData.filter(({firstname, lastname }) => {
-
+            
             if(firstname.includes(target.value) || lastname.includes(target.value)){
                 return true
             }
-
+            
         })
-
+        
         renderAgents(filteredData)
-
+        
         
     });
     
@@ -65,17 +86,17 @@ export const Agents = () => {
     const agentForm = container.querySelector('#agentForm');
     agentForm.addEventListener('submit', (e) => {
         e.preventDefault();
-
+        
         let { elements } = e.target;
         let data = {
             firstname : elements[0].value,
             lastname: elements[1].value
         };
-
+        
         if(data.firstname.length <= 0 || data.lastname.length <= 0){
             alert('Insert valid data');
         }else{
-
+            
             let url = 'http://localhost:3000/newAgent';
             axios({
                 method: 'post',
@@ -87,10 +108,10 @@ export const Agents = () => {
             }, 2000)
             
         }
-
+        
     });
 
-
+    
     return container;
-
+    
 }
